3b457255be4a323f42f06cc09c0dfc28886d92b7
Harshada Tupe <harshadatupe8@gmail.com>
2025-01-27 21:39:44 -0800

    completed PreCourse-1

--- Exercise_2.py
+++ Exercise_2.py
@@ -5,13 +5,28 @@ class Node:
        self.next = None
  
 class Stack:
+    # tc O(1), for all operations, sc O(n).
     def __init__(self):
+        self.head = None
         
     def push(self, data):
+        if not self.head:
+            self.head = Node(data)
+        else:
+            node = Node(data)
+            node.next = self.head
+            self.head = node
         
     def pop(self):
+        if not self.head:
+            return None
+        top_of_stack = self.head
+        self.head = self.head.next
+        return top_of_stack
+
         
 a_stack = Stack()
+
 while True:
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     print('push <value>')
--- Exercise_3.py
+++ Exercise_3.py
@@ -17,6 +17,15 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        if not self.head:
+            self.head = ListNode(data)
+            return
+        
+        prev, curr = None, self.head
+        while curr:
+            prev = curr
+            curr = curr.next
+        prev.next = ListNode(data)
         
     def find(self, key):
         """
@@ -24,9 +33,26 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        curr = self.head
+        while curr:
+            if curr.val == key:
+                return curr
+            curr = curr.next
+        
+        return None
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        prev, curr = None, self.head
+        while curr:
+            if curr.val == key:
+                if not prev:
+                    self.head = self.head.next
+                else:
+                    prev.next = curr.next
+                return
+            prev = curr
+            curr = curr.next
